14c69ae3506225c9087902f9571953cddd238b92
Anjasputra012 <anjasputra012@gmail.com>
2024-04-25 13:15:41 +0700

    third

--- resources/js/pages/landing/home/product-card.jsx
+++ resources/js/pages/landing/home/product-card.jsx
@@ -4,33 +4,29 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { formatRupiah } from '@/lib/utils';
 import { Image } from '@/components/image';
 import { Badge } from '@/components/ui/badge';
+import { Link } from '@inertiajs/react';
 
 const ProductCard = ({ product }) => {
     return (
         <Card key={product.id}>
-            <CardHeader>
-                <CardTitle>{product.name}</CardTitle>
-                <CardDescription>
-                <p className='mb-3'>{product.vendor.name}</p>
+            <Link href={route('landing.products.show', product.slug)}>
+                <CardHeader>
+                    <CardTitle>{product.name}</CardTitle>
+                    <CardDescription>
+                        <p className="mb-3">{product.vendor.name}</p>
 
-                   <Badge>
-                        {formatRupiah(product.price)}
-                    </Badge>
-                </CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Image
-                    className="aspect-[16/9] border w-full rounded-2xl object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
-                    src={`${product.image}`}
-                ></Image>
-            </CardContent>
-            <CardFooter>
-                <div className="flex items-center justify-between gap-2">
-               
-                </div>
-            </CardFooter>
+                        <Badge>{formatRupiah(product.price)}</Badge>
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Image className="aspect-[16/9] w-full rounded-2xl border object-cover sm:aspect-[2/1] lg:aspect-[3/2]" src={`${product.image}`}></Image>
+                </CardContent>
+                <CardFooter>
+                    <div className="flex items-center justify-between gap-2"></div>
+                </CardFooter>
+            </Link>
         </Card>
     );
-}
+};
 
 export default ProductCard;
