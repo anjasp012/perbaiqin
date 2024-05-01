54ecd209182f2c41549c6f79f2053720d766be19
Anjasputra012 <anjasputra012@gmail.com>
2024-05-01 14:55:47 +0700

    consultation pending

--- resources/js/pages/technician/consultation/index.jsx
+++ resources/js/pages/technician/consultation/index.jsx
@@ -9,91 +9,96 @@ import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import Pagination from '@/shared/pagination';
 import { User as UserIcon } from 'lucide-react';
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Badge } from '@/components/ui/badge';
 import { formatDateTime } from '@/lib/utils';
 
 const TechnicianConsultationIndex = ({ consultations }) => {
-  return (
-    <Container>
-      <Head title='Consultation'></Head>
-      <Header title={`Consultations`} subtitle={'Consultations request list'} />
-      <div className='mt-5'>
-        <div className="px-4 py-6 sm:px-6 lg:p-8">
-          <div className="mb-8 flex justify-between">
-            <Search URL={route('technician.consultations.index')} />
-          </div>
+    return (
+        <Container>
+            <Head title="Consultation"></Head>
+            <Header title={`Consultations`} subtitle={'Consultations request list'} />
+            <div className="mt-5">
+                <div className="px-4 py-6 sm:px-6 lg:p-8">
+                    <div className="mb-8 flex justify-between">
+                        <Search URL={route('technician.consultations.index')} />
+                    </div>
 
-          <Card className="border-none">
-            <CardHeader></CardHeader>
-            <CardContent>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>No.</TableHead>
-                    <TableHead>Clients</TableHead>
-                    <TableHead>Status</TableHead>
-                    <TableHead>Created At</TableHead>
-                    <TableHead>Actions</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {consultations.data.map((consultation, index) => (
-                    <TableRow key={index}>
-                      <TableCell>{++index + (consultations.current_page - 1) * consultations.per_page}</TableCell>
-                      <TableCell className="flex items-center justify-between">
-                        <div className="flex items-center">
-                          <Avatar className="mr-2">
-                            <AvatarImage
-                              src={consultation.user.avatar}
-                              alt={consultation.user.name}
-                              className="h-6 w-6 rounded-full border-2"
-                            />
-                            <AvatarFallback>
-                              <UserIcon />
-                            </AvatarFallback>
-                          </Avatar>
-                          <span>{consultation.user.name}</span>
-                        </div>
-                      </TableCell>
+                    <Card className="border-none">
+                        <CardHeader></CardHeader>
+                        <CardContent>
+                            <Table>
+                                <TableHeader>
+                                    <TableRow>
+                                        <TableHead>No.</TableHead>
+                                        <TableHead>Clients</TableHead>
+                                        <TableHead>Status</TableHead>
+                                        <TableHead>Created At</TableHead>
+                                        <TableHead>Actions</TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody>
+                                    {consultations.data.map((consultation, index) => (
+                                        <TableRow key={index}>
+                                            <TableCell>{++index + (consultations.current_page - 1) * consultations.per_page}</TableCell>
+                                            <TableCell className="flex items-center justify-between">
+                                                <div className="flex items-center">
+                                                    <Avatar className="mr-2">
+                                                        <AvatarImage
+                                                            src={consultation.user.avatar}
+                                                            alt={consultation.user.name}
+                                                            className="h-6 w-6 rounded-full border-2"
+                                                        />
+                                                        <AvatarFallback>
+                                                            <UserIcon />
+                                                        </AvatarFallback>
+                                                    </Avatar>
+                                                    <span>{consultation.user.name}</span>
+                                                </div>
+                                            </TableCell>
 
-                      <TableCell><Badge>{consultation.status}</Badge></TableCell>
-                      <TableCell>{formatDateTime(consultation.created_at)}</TableCell>
-                      <TableCell>
-                        <div className="flex gap-2">
-                          <Link href={route('technician.consultations.chat', consultation.id)} className={buttonVariants({ size:'sm', variant: 'secondary' })}>
-                            Go to Chat
-                          </Link>
-                          <Button
-                            size="sm"
-                            variant="destructive"
-                            onClick={() =>
-                              ask({
-                                url: route('technician.consultations.index'),
-                                method: 'delete',
-                                icon: 'warning',
-                                message: 'Are you sure you want to change to completed?',
-                              })
-                            }
-                          >
-                            <span>Change Status</span>
-                          </Button>
-                        </div>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
+                                            <TableCell>
+                                                <Badge>{consultation.status}</Badge>
+                                            </TableCell>
+                                            <TableCell>{formatDateTime(consultation.created_at)}</TableCell>
+                                            <TableCell>
+                                                <div className="flex gap-2">
+                                                    {consultation.status == 'ongoing' && (
+                                                        <Link
+                                                            href={route('technician.consultations.chat', consultation.id)}
+                                                            className={buttonVariants({ size: 'sm', variant: 'secondary' })}
+                                                        >
+                                                            Go to Chat
+                                                        </Link>
+                                                    )}
+                                                    <Button
+                                                        size="sm"
+                                                        variant="destructive"
+                                                        onClick={() =>
+                                                            ask({
+                                                                url: route('technician.consultations.index'),
+                                                                method: 'delete',
+                                                                icon: 'warning',
+                                                                message: 'Are you sure you want to change to completed?',
+                                                            })
+                                                        }
+                                                    >
+                                                        <span>Change Status</span>
+                                                    </Button>
+                                                </div>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
 
-              <Pagination links={consultations.links} />
-            </CardContent>
-          </Card>
-        </div>
-
-
-      </div>
-    </Container>
-  );
+                            <Pagination links={consultations.links} />
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
+        </Container>
+    );
 };
 
 export default TechnicianConsultationIndex;
--- resources/js/pages/technician/consultation/message-form.jsx
+++ resources/js/pages/technician/consultation/message-form.jsx
@@ -22,31 +22,34 @@ const MessageForm = ({ handleSubmit, newMessage, image, setImage, setNewMessage,
     };
 
     return (
-        <form onSubmit={handleSubmit} className="flex  justify-between h-full">
+        <form onSubmit={handleSubmit} className="flex  h-full justify-between">
             <div className="flex items-center ">
-                <div className="flex-shrink-0 relative">
-                    {image && (
-                        <div className="relative w-12 h-12">
+                {image ? (
+                    <div className="relative flex-shrink-0">
+                        <div className="relative h-12 w-12">
                             <img src={URL.createObjectURL(image)} alt="Selected Image" className="h-full w-full rounded-md" />
                             <Button
                                 type="button"
                                 size="icon"
                                 onClick={cancelImageSelection}
-                                className="absolute right-0 top-0 rounded-full p-1 shadow focus:outline-none"
+                                className="absolute right-1/2 top-1/2 -translate-y-1/2 translate-x-1/2 rounded-full p-1 shadow focus:outline-none"
                             >
                                 <X size={16} />
                             </Button>
                         </div>
-                    )}
-                </div>
-                <label className="flex items-center cursor-pointer">
-                    <input ref={inputFileRef} type="file" className="hidden" accept="image/*" onChange={handleChange} name="image" />
-                    <span className="cursor-pointer rounded-md   text-gray-600"><Paperclip /></span>
-                </label>
+                    </div>
+                ) : (
+                    <label className="flex cursor-pointer items-center">
+                        <input ref={inputFileRef} type="file" className="hidden" accept="image/*" onChange={handleChange} name="image" />
+                        <span className="cursor-pointer rounded-md   text-gray-600">
+                            <Paperclip />
+                        </span>
+                    </label>
+                )}
             </div>
-            <div className='w-full flex items-center mx-2'>
-            <Input
-                    className="flex-grow resize-none w-full rounded-md p-2 ml-2"
+            <div className="mx-2 flex w-full items-center">
+                <Input
+                    className="ml-2 w-full flex-grow resize-none rounded-md p-2"
                     placeholder="Type your message..."
                     value={newMessage}
                     name="message"
@@ -54,7 +57,6 @@ const MessageForm = ({ handleSubmit, newMessage, image, setImage, setNewMessage,
                 />
             </div>
             <div className="flex items-center justify-between">
-
                 <Button type="submit" variant="default" className={`${loading ? 'cursor-not-allowed opacity-50' : ''}`}>
                     <Send className="me-2 h-4 w-4" />
                     {loading ? 'Sending...' : 'Send'}
