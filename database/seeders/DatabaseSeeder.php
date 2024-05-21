761e6ff270d532b1af0757a23e6daef3153126fa
Anjasputra012 <anjasputra012@gmail.com>
2024-05-21 09:57:53 +0700

    filter

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
+            // UserSeeder::class,
             AdminSeeder::class,
-            TechnicianSeeder::class,
-            VendorSeeder::class,
+            // TechnicianSeeder::class,
+            // VendorSeeder::class,
         ]);
     }
 }
